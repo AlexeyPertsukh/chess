f72283d741278b3778b146029af57ca72ddcc2a8
Alexey Pertsukh <85017966+AlexeyPertsukh@users.noreply.github.com>
2022-08-29 21:11:42 +0300

    update test

--- src/test/java/com/company/model/artificial_intelligence/BasicAiTest.java
+++ src/test/java/com/company/model/artificial_intelligence/BasicAiTest.java
@@ -7,6 +7,7 @@ import com.company.model.figure.FigureColor;
 import org.junit.jupiter.api.Test;
 import test_util.BoardUtil;
 import test_util.PrintUtil;
+import test_util.triple.Triple;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -18,17 +19,31 @@ class BasicAiTest {
 
     @Test
     void test() {
-        FigureColor myColor = FigureColor.WHITE;
-        Board board = BoardUtil.boardOf("pbb3, qwe3, nwd5, qba7, kbe7, pwf6, kwf2");
-        Danger danger = new Danger(board, myColor);
-        PrintUtil.printPrimitiveBoard(board);
-        PrintUtil.print(danger);
-        PrintUtil.printBoard(board);
 
-        Iai ai = new BasicAi(board);
-        Way way = ai.getBestMove(danger, myColor);
-        board.transfer(way);
-        PrintUtil.printBoard(board);
+//        Triple[] test = new Triple[]{
+//                Triple.of("pbb3, qwe3, nwd5, qba7, kbe7, pwf6, kwf2", "f6"),
+//                Triple.of("kbg7, rwe8", "e8"),
+//        };
+//
+//        FigureColor myColor = FigureColor.WHITE;
+//        FigureColor saveKingColor = FigureColor.BLACK;
+//
+//        for (Triple t : test) {
+//            Board board = BoardUtil.boardOf((String) t.first);
+//
+//        }
+//
+//
+//        Board board = BoardUtil.boardOf("pbb3, qwe3, qba7, kbe7, pwf6, kwf2");
+//        Danger danger = new Danger(board, myColor);
+//        PrintUtil.printPrimitiveBoard(board);
+//        PrintUtil.print(danger);
+//        PrintUtil.printBoard(board);
+//
+//        Iai ai = new BasicAi(board);
+//        Way way = ai.getBestMove(danger, saveKingColor);
+//        board.transfer(way);
+//        PrintUtil.printBoard(board);
 
     }
 
