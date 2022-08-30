c786e6fc1904ad8632d2bc0503d30483e1babbfe
Alexey Pertsukh <85017966+AlexeyPertsukh@users.noreply.github.com>
2022-08-30 20:05:25 +0300

    add test exception on Casting

--- src/test/java/com/company/service/move/CastlingTest.java
+++ src/test/java/com/company/service/move/CastlingTest.java
@@ -1,7 +1,7 @@
 package com.company.service.move;
 
 import com.company.model.board.Board;
-import com.company.model.board.BoardHelper;
+import com.company.model.chess_exception.ChessException;
 import com.company.model.command.Command;
 import com.company.model.command.CommandEnum;
 import com.company.model.danger.Danger;
@@ -16,12 +16,17 @@ import static org.junit.jupiter.api.Assertions.*;
 class CastlingTest {
 
     @Test
-    void exceptionOnCastlingToUnderAttack() {
+    void exceptionOnCastlingToUnderAttackTrue() {
         Team white = Team.WHITE;
         Team black = Team.BLACK;
 
+        String stringCommand = CommandEnum.RIGHT_CASTLING.getKey();
+
         Triple[] test = new Triple[]{
-                Triple.of("kbe8, rbh8, rba8, rwg1", black, CommandEnum.RIGHT_CASTLING.getKey()),
+                Triple.of("kbe8, rbh8, rba8, rwg1", black, stringCommand),
+                Triple.of("kbe8, rbh8, rba8, nwh6", black, stringCommand),
+                Triple.of("kbe8, rbh8, rba8, pwh7", black, stringCommand),
+                Triple.of("kbe8, rbh8, rba8, bwd6", black, stringCommand),
         };
 
         for (Triple t : test) {
@@ -30,13 +35,43 @@ class CastlingTest {
 
             String string = (String) t.first;
             Board board = BoardUtil.boardOf(string);
-            Command command = new Command((String) t.second);
+            Command command = new Command(stringCommand);
             Danger danger = new Danger(board, team);
 
             Move castling = new Castling(board);
 
-            castling.execute(command, player, danger);
-//            assertFalse(actual);
+            assertThrows(ChessException.class, () -> {
+                castling.execute(command, player, danger);
+            });
+
+        }
+    }
+
+    @Test
+    void exceptionOnCastlingToUnderAttackFalse() {
+        Team white = Team.WHITE;
+        Team black = Team.BLACK;
+        String stringCommand = CommandEnum.LEFT_CASTLING.getKey();
+
+        Triple[] test = new Triple[]{
+                Triple.of("kbe8, rbh8, rba8, rwg1", black, stringCommand),
+                Triple.of("kbe8, rbh8, rba8, nwh6", black, stringCommand),
+                Triple.of("kbe8, rbh8, rba8, pwh7", black, stringCommand),
+        };
+
+        for (Triple t : test) {
+            Team team = (Team) t.second;
+            Player player = new Player(team);
+
+            String string = (String) t.first;
+            Board board = BoardUtil.boardOf(string);
+            Command command = new Command(stringCommand);
+            Danger danger = new Danger(board, team);
+
+            Move castling = new Castling(board);
+
+            castling.execute(command, player, danger);  //нет исключения- все ок
+
         }
     }
 
